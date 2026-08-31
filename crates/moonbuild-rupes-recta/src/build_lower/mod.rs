6c2cc9545951bbf0aba84bd2275996172e651fed
Guest0x0 <guest0x0@outlook.com>
2026-08-31 14:43:02 +0800

    enable `/std:c11` for MSVC

--- crates/moonbuild-rupes-recta/src/build_lower/mod.rs
+++ crates/moonbuild-rupes-recta/src/build_lower/mod.rs
@@ -1134,6 +1134,11 @@ mod tests {
             runtime_compile_command,
             "native/debug/build/runtime-runtime.obj"
         ));
+        assert!(
+            runtime_compile_command
+                .iter()
+                .any(|arg| arg == moonutil::compiler_flags::WINDOWS_MSVC_C_STANDARD_FLAG)
+        );
 
         let runtime_archive_command = lowered
             .command_args_by_output
@@ -1164,6 +1169,11 @@ mod tests {
             stub_compile_command
                 .iter()
                 .any(|arg| arg == moonutil::compiler_flags::WINDOWS_MSVC_STATIC_RUNTIME_FLAG)
+        );
+        assert!(
+            stub_compile_command
+                .iter()
+                .any(|arg| arg == moonutil::compiler_flags::WINDOWS_MSVC_C_STANDARD_FLAG)
         );
 
         let moonc_inputs = n2_input_paths_for_command(&lowered, |command| {
